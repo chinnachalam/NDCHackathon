858f5195a0fce90156b4664732f8ce2eb085225a
chinna vadada <chinna.vadada@klm.com>
2017-10-10 17:07:29 +0200

    removed commented code

--- ndc/ndc-rest/src/main/java/tcs/ndc/hackathon/ndcrest/controllers/RestController.java
+++ ndc/ndc-rest/src/main/java/tcs/ndc/hackathon/ndcrest/controllers/RestController.java
@@ -25,38 +25,12 @@ public class RestController {
     @RequestMapping(value = "/offers", method = RequestMethod.POST)
     @ResponseBody
     public void offers(@RequestBody Offer offer) {
-        /*GregorianCalendar gc = new GregorianCalendar();
-        gc.set(GregorianCalendar.DAY_OF_MONTH, 25);
-        gc.set(GregorianCalendar.MONTH, GregorianCalendar.DECEMBER);
-        gc.set(GregorianCalendar.YEAR, 2017);
-
-        AirShoppingRQBuilder builder = new AirShoppingRQBuilder();
-        builder.addTravelAgencySender("Test agency", "98417900", "test agent");
-        builder.addOriginDestination("ARN", "LHR", gc.getTime());
-        builder.addAirlinePreference("C9");
-        builder.addCabinPreference("M");
-
-        AirShoppingRQ request = builder.build();*/
-
-        // NDCConsumer client = new NDCConsumer("http://iata.api.mashery.com/athena/ndc162api", "2be3rbjvuj32peygqcwp6fy5");
+       /* NDCConsumer client = new NDCConsumer("http://iata.api.mashery.com/athena/ndc162api", "2be3rbjvuj32peygqcwp6fy5");*/
         NDCConsumer client = new NDCConsumer("http://iata.api.mashery.com/kronos/ndc162api", "2be3rbjvuj32peygqcwp6fy5");
-        AirShoppingRS response = null;
-
-       /* LinkedList<CurrCode> curr = new LinkedList<CurrCode>();
-
-        CurrCode ccode = new CurrCode();
-        ccode.setId("USD");
-        ccode.setValue("USD");
-        curr.add(ccode);
-
-        AirShopReqParamsType t = new AirShopReqParamsType();
-        t.setCurrCodes(curr);
-
-        request.setParameters(t);*/
 
         try {
             AirShoppingRQ airShoppingRQ = airShoppingRQMapper.map(offer);
-            response = client.airShopping(airShoppingRQ);
+            AirShoppingRS response = client.airShopping(airShoppingRQ);
 
             try {
                 System.out.println("SuccessType:" + response.getSuccess());
