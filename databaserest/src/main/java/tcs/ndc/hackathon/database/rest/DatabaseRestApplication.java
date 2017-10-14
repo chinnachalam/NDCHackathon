95d700b46b303f375ebac6b648e45b2e6651826e
chinna vadada <chinna.vadada@klm.com>
2017-10-14 11:46:40 +0200

    added heroku

--- databaserest/src/main/java/tcs/ndc/hackathon/database/rest/DatabaseRestApplication.java
+++ databaserest/src/main/java/tcs/ndc/hackathon/database/rest/DatabaseRestApplication.java
@@ -2,6 +2,7 @@ package tcs.ndc.hackathon.database.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class DatabaseRestApplication {
