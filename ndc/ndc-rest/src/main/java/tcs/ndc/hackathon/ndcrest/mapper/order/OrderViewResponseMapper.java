68666f3ffcad8bc3697aeebdf3b8b2fb4091bf32
chinna vadada <chinna.vadada@klm.com>
2017-10-16 20:22:44 +0200

    added annotations

--- ndc/ndc-rest/src/main/java/tcs/ndc/hackathon/ndcrest/mapper/order/OrderViewResponseMapper.java
+++ ndc/ndc-rest/src/main/java/tcs/ndc/hackathon/ndcrest/mapper/order/OrderViewResponseMapper.java
@@ -1,8 +1,10 @@
 package tcs.ndc.hackathon.ndcrest.mapper.order;
 
 import org.iata.ndc.schema.OrderViewRS;
+import org.springframework.stereotype.Component;
 import tcs.ndc.hackathon.ndcrest.model.order.OrderView;
 
+@Component
 public class OrderViewResponseMapper {
 
     public OrderView map(OrderViewRS orderViewRS) {
--- ndc/ndc-rest/src/main/java/tcs/ndc/hackathon/ndcrest/service/OrderService.java
+++ ndc/ndc-rest/src/main/java/tcs/ndc/hackathon/ndcrest/service/OrderService.java
@@ -4,26 +4,28 @@ import org.iata.ndc.schema.OrderCreateRQ;
 import org.iata.ndc.schema.OrderListRS;
 import org.iata.ndc.schema.OrderViewRS;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import tcs.ndc.hackathon.ndccore.NDCConsumer;
 import tcs.ndc.hackathon.ndcrest.mapper.core.OrderCreateRQMapper;
 import tcs.ndc.hackathon.ndcrest.mapper.order.OrderViewResponseMapper;
 import tcs.ndc.hackathon.ndcrest.model.order.OrderCreate;
 import tcs.ndc.hackathon.ndcrest.model.order.OrderView;
 
+@Component
 public class OrderService {
 
     @Autowired OrderCreateRQMapper orderCreateRQMapper;
     @Autowired NDCConsumer ndcConsumer;
     @Autowired OrderViewResponseMapper orderViewResponseMapper;
-    @Autowired OrderView orderView;
-
 
     public OrderView createOrder(OrderCreate orderCreate) {
+        OrderView orderView = null;
         try {
-        OrderCreateRQ orderCreateRQ = orderCreateRQMapper.buildOrderCreateRQ(orderCreate);
-        OrderViewRS response = ndcConsumer.orderCreate(orderCreateRQ);
-        orderView = orderViewResponseMapper.map(response);
-        } catch(Exception e) {
+            OrderCreateRQ orderCreateRQ = orderCreateRQMapper.buildOrderCreateRQ(orderCreate);
+            OrderViewRS response = ndcConsumer.orderCreate(orderCreateRQ);
+            orderView = orderViewResponseMapper.map(response);
+        }
+        catch (Exception e) {
             e.printStackTrace();
         }
         return orderView;
